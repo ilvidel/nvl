ec34535ceebd905568a2fda3e9f1922755d6094f
ilvidel <ignacio.diez.arias@gmail.com>
2024-12-03 08:05:53 +0000

    more charts

--- generate_charts.py
+++ generate_charts.py
@@ -1,4 +1,5 @@
 import statistics
+from collections import Counter
 
 import pandas
 import plotly.express as px
@@ -27,10 +28,9 @@ class MyPlotter:
         for game in self.games:
             total = sum([int(x) for x in game.home_points + game.away_points if x.isnumeric()])
             if total < 75:
-                # print(f"Date: {game.date()} Home: {game.home} ({game.home_points}) Away: {game.away} ({game.away_points})")
+                print(
+                    f"{game.division} {game.category} Date: {game.date()} Home: {game.home} ({game.home_points}) Away: {game.away} ({game.away_points})")
                 continue
-            if total > 250:
-                print(f"Date: {game.date()} Home: {game.home} ({game.home_points}) Away: {game.away} ({game.away_points})")
             points.append(total)
             sex.append(game.category)
 
@@ -49,7 +49,80 @@ class MyPlotter:
         fig.update_traces(opacity=0.75)
         fig.show()
 
+    def plot_home_victories(self):
+        """Percentage of home victories vs away victories"""
+        results = []
+        for g in self.games:
+            r = {'home': g.home_sets, 'away': g.away_sets, 'division': g.division, 'category': g.category}
+            results.append(r)
+
+        home_victories = len(list(filter(lambda g: g['home'] > g['away'], results)))
+        away_victories = len(list(filter(lambda g: g['home'] < g['away'], results)))
+
+        df = pandas.DataFrame(dict(
+            where=["Home", "Away"],
+            count=[home_victories, away_victories]
+        ))
+        fig = px.pie(df, values='count', names='where', title="Home vs Away victories", hole=.5)
+        fig.show()
+
+    def plot_home_victories_per_division(self):
+        """Percentage of home victories vs away victories"""
+        results = []
+        for g in self.games:
+            r = {'home': g.home_sets, 'away': g.away_sets, 'division': g.division, 'category': g.category}
+            results.append(r)
+
+        data = []
+        divs = ['superleague', 'division_1', 'division_2', 'division_3', 'cup', 'shield']
+        for div in divs:
+            total = len(list(filter(lambda f: f['division'] == div, results)))
+            home_victories = len(
+                list(filter(lambda g: g['home'] > g['away'] and g['division'] == div, results))) / total
+            away_victories = 1 - home_victories
+            data.extend([home_victories, away_victories])
+            # data.extend(
+            #     [len(list(filter(lambda g: g['home'] > g['away'] and g['division'] == div, results))),
+            #      len(list(filter(lambda g: g['home'] < g['away'] and g['division'] == div, results)))]
+            # )
+
+        df = pandas.DataFrame(dict(
+            where=["Home", "Away", "Home", "Away", "Home", "Away", "Home", "Away", "Home", "Away", "Home", "Away"],
+            divisions=[
+                "Super", "Super", "Div1", "Div1", "Div2", "Div2", "Div3", "Div3", "Cup", "Cup", "Shield", "Shield"],
+            count=data
+        ))
+        fig = px.bar(
+            df, x='count', y='divisions',
+            color='divisions', pattern_shape='where',
+            title="Home vs Away victories per division"
+        )
+        fig.show()
+
+    def plot_number_of_games(self):
+        """Number of games played per year"""
+        years = [g.timestamp.year for g in self.games]
+        fig = px.histogram(years, title="Number of games per year")
+        fig.update_layout(bargap=.2)
+        fig.show()
+
+    def plot_number_of_games_per_division(self):
+        """Number of games played per year, per division"""
+        years = [g.timestamp.year for g in self.games]
+
+        for y in years:
+            super = [x.division for x in filter(lambda g: g.timestamp.year==y, self.games)]
+
+        df = pandas.DataFrame(dict(count=count, years=years))
+        fig = px.bar(df, x='years', y='count', color='count', color_continuous_scale='sunsetdark',
+                     title="Number of games per year, per division")
+        fig.show()
+
 
 if __name__ == "__main__":
     plotter = MyPlotter()
-    plotter.plot_total_points()
+    # plotter.plot_total_points()
+    # plotter.plot_home_victories()
+    # plotter.plot_home_victories_per_division()
+    # plotter.plot_number_of_games()
+    plotter.plot_number_of_games_per_division()  # TODO
